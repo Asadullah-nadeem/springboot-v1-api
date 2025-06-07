3c72b1f84c062e07f65e79ced7b734f91e2e44bb
Muhammad <asadullahnadeem48@gmail.com>
2025-06-07 21:02:59 +0530

    springboot-v1-api Add code

---
+++ src/main/java/com/api/Api/v1/HelloController.java
@@ -0,0 +1,18 @@
+package com.api.Api.v1;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public Map<String, String> Hello() {
+    String V1 = "Massage", Vnmae = "Hello World";
+      return Map.of("Message", "Hello World");
+  }
+
+}
