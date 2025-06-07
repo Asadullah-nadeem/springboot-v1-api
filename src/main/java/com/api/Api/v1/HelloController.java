b3ccc810dccdefa4a602b5dd9b75f4e40f74c137
Muhammad <asadullahnadeem48@gmail.com>
2025-06-07 21:08:57 +0530

    update code '2'

--- src/main/java/com/api/Api/v1/HelloController.java
+++ src/main/java/com/api/Api/v1/HelloController.java
@@ -1,19 +1,26 @@
 package com.api.Api.v1;
 
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// import java.util.Map;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+
+// @RestController
+// public class HelloController {
+
+//   @GetMapping("/")
+//   public Map<String, String> Hello() {
+//     String V1 = "Massage", Vnmae = "Hello World";
+//       // return Map.of("Message", "Hello World");
+//       return Map.of(V1, Vnmae);
+//   }
+
+// }
 
 @RestController
-public class HelloController {
-
-  @GetMapping("/")
-  public Map<String, String> Hello() {
-    String V1 = "Massage", Vnmae = "Hello World";
-      // return Map.of("Message", "Hello World");
-      return Map.of(V1, Vnmae);
-  }
-
+public class HelloController{
+  
 }
---
+++ src/main/java/com/api/Api/v1/User.java
@@ -0,0 +1,9 @@
+package com.api.Api.v1;
+
+public class User {
+  private int id;
+  private String Name;
+  private String Message;
+
+  // Constructor
+}
